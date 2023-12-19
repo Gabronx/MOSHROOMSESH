9b024225d69ffb5c6f03093a11074fc9dee88c76
Fabio Gabriel <fabiogabriel.work@hotmail.com>
2023-12-19 11:51:42 -0300

    subindo arquivos

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  const darkModeToggle = document.getElementById('dark-mode-toggle');
+  const body = document.body;
+
+  darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+  });
+</script>
