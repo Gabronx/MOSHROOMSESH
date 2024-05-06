6917cf99fd3037d3ec33c2ead47c8c41df9a6768
gianlucasms <gianlucasmds17@gmail.com>
2024-05-06 13:12:04 -0300

    some adjustes, just see the changes...

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
   $(window).scroll(function() {
       var scroll = $(window).scrollTop(),
           speed = 0.5;
-
-      // Ajusta a posição do background para começar do meio e mover para baixo
       $('.main-image').css('background-position', 'center calc(50% + ' + (scroll * speed) + 'px)');
   });
 });
@@ -18,7 +16,6 @@ $(document).ready(function() {
           var thisHeight = $(this).outerHeight();
           var thisBottom = thisTop + thisHeight;
 
-          // Se parte da imagem está visível
           if (thisBottom > scrollPos && thisTop < (scrollPos + windowHeight)) {
               $(this).find('.dark-overlay').css('background', 'rgba(0, 0, 0, 0)');
           } else {
@@ -27,7 +24,6 @@ $(document).ready(function() {
       });
   }
 
-  // Verifica ao carregar e ao rolar a página
   checkVisibility();
   $(window).scroll(function() {
       checkVisibility();
